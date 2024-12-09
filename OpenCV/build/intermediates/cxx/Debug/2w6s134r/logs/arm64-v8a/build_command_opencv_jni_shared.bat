b160a5a65146bf53851c1a8130706e7a584f5c49
Sergio Sanchez <sergetoos@gmail.com>
2024-12-09 11:19:43 -0600

    Gradle/Kotlin/OpenCV Libraries
    Added Essential Libraries to Remote Repo.
    -Main source code was omitted.

---
+++ OpenCV/build/intermediates/cxx/Debug/2w6s134r/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\serge\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\serge\\AndroidStudioProjects\\ImGEN1000\\OpenCV\\.cxx\\Debug\\2w6s134r\\arm64-v8a" ^
+  opencv_jni_shared
---
+++ OpenCV/build/intermediates/cxx/RelWithDebInfo/53594o1h/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\serge\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\serge\\AndroidStudioProjects\\ImGEN1000\\OpenCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\serge\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\serge\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\serge\\AppData\\Local\\Android\\Sdk\\ndk\\26.1.10909125\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\serge\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\serge\\AndroidStudioProjects\\ImGEN1000\\OpenCV\\build\\intermediates\\cxx\\RelWithDebInfo\\53594o1h\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\serge\\AndroidStudioProjects\\ImGEN1000\\OpenCV\\build\\intermediates\\cxx\\RelWithDebInfo\\53594o1h\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BC:\\Users\\serge\\AndroidStudioProjects\\ImGEN1000\\OpenCV\\.cxx\\RelWithDebInfo\\53594o1h\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
